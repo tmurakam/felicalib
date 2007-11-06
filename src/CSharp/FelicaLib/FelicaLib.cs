42f268a8f40451f7007ae8d2e9a878743c701af5
tmurakam <tmurakam@270afa9f-293e-0410-a133-a460c3dcb273>
2007-11-06 14:56:54 +0000

    add CSharp
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/felicalib/trunk@33 270afa9f-293e-0410-a133-a460c3dcb273

---
+++ src/CSharp/FelicaLib/FelicaLib.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace FelicaLib
+{
+    public class FelicaLib
+    {
+	[DllImport("felicalib.dll")]
+	private extern static IntPtr pasori_open(String dummy);
+	[DllImport("felicalib.dll")]
+	private extern static void pasori_close(IntPtr p);
+	[DllImport("felicalib.dll")]
+	private extern static int pasori_init(IntPtr p);
+	[DllImport("felicalib.dll")]
+	private extern static IntPtr felica_polling(IntPtr p, ushort systemcode, byte rfu, byte time_slot);
+	[DllImport("felicalib.dll")]
+	private extern static int felica_read_without_encryption02(IntPtr f, int systemcode, int mode, byte addr, byte[] data);
+	[DllImport("felicalib.dll")]
+	private extern static void felica_free(IntPtr f);
+
+	private IntPtr pasorip = IntPtr.Zero;
+	private IntPtr felicap = IntPtr.Zero;
+
+	FelicaLib()
+	{
+	    pasorip = pasori_open(null);
+	    if (pasorip == IntPtr.Zero)
+	    {
+		throw new Exception("can't load felicalib library");
+	    }
+	    if (pasori_init(pasorip) != 0)
+	    {
+		throw new Exception("can't connect PaSoRi");
+	    }
+	}
+
+	~FelicaLib()
+	{
+	    pasori_close(pasorip);
+	}
+
+	void Polling(int systemcode)
+	{
+	    felica_free(felicap);
+
+	    felicap = felica_polling(pasorip, (ushort)systemcode, 0, 0);
+	    if (felicap == IntPtr.Zero)
+	    {
+		throw new Exception("polling card failed");
+	    }
+	}
+
+	byte[] ReadWithoutEncryption(int systemcode, int addr)
+	{
+	    if (felicap == IntPtr.Zero)
+	    {
+		throw new Exception("no polling executed.");
+	    }
+
+	    byte[] data = new byte[16];
+	    int ret = felica_read_without_encryption02(felicap, systemcode, 0, (byte)addr, data);
+	    if (ret != 0)
+	    {
+		return null;
+	    }
+	    return data;
+	}
+    }
+}
