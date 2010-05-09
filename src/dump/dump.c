273977044f25fb47f834477cebc2d7b2f1f24856
Takuya Murakami <tmurakam@tmurakam.org>
2010-05-09 23:26:22 +0900

    merge x64/unicode

--- src/dump/dump.c
+++ src/dump/dump.c
@@ -37,74 +37,88 @@
 */
 
 #include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+#include <locale.h>
 
 #include "felicalib.h"
 
 static void printserviceinfo(uint16 s);
-static void hexdump(uint8 *addr, int n);
+static void hexdump(uint8* addr, int n);
 
 int _tmain(int argc, _TCHAR *argv[])
 {
-    pasori *p;
-    felica *f, *f2;
+    pasori* p;
+    felica  *f, *f2;
     int i, j, k;
 
+    setlocale(LC_ALL, "Japanese");
+
     p = pasori_open(NULL);
-    if (!p) {
-        fprintf(stderr, "PaSoRi open failed.\n");
+    if (!p)
+    {
+        _ftprintf(stderr, _T("PaSoRi open failed.\n"));
         exit(1);
     }
     pasori_init(p);
     
     f = felica_polling(p, POLLING_ANY, 0, 0);
-    if (!f) {
-        fprintf(stderr, "Polling card failed.\n");
+    if (!f)
+    {
+        _ftprintf(stderr, _T("Polling card failed.\n"));
         exit(1);
     }
-    printf("# IDm: ");
+    _tprintf(_T("# IDm: "));
     hexdump(f->IDm, 8);
-    printf("\n");
-    printf("# PMm: ");
+    _tprintf(_T("\n"));
+    _tprintf(_T("# PMm: "));
     hexdump(f->PMm, 8);
-    printf("\n\n");
+    _tprintf(_T("\n\n"));
     felica_free(f);
 
     f = felica_enum_systemcode(p);
-    if (!f) {
+    if (!f)
+    {
         exit(1);
     }
 
-    for (i = 0; i < f->num_system_code; i++) {
-        printf("# System code: %04X\n", N2HS(f->system_code[i]));
+    for (i = 0; i < f->num_system_code; i++)
+    {
+        _tprintf(_T("# System code: %04X\n"), N2HS(f->system_code[i]));
         f2 = felica_enum_service(p, N2HS(f->system_code[i]));
-        if (!f2) {
-            fprintf(stderr, "Enum service failed.\n");
+        if (!f2)
+        {
+            _ftprintf(stderr, _T("Enum service failed.\n"));
             exit(1);
         }
         
-        printf("# Number of area = %d\n", f2->num_area_code);
-        for (j = 0; j < f2->num_area_code; j++) {
-            printf("# Area: %04X - %04X\n", f2->area_code[j], f2->end_service_code[j]);
+        _tprintf(_T("# Number of area = %d\n"), f2->num_area_code);
+        for (j = 0; j < f2->num_area_code; j++)
+        {
+            _tprintf(_T("# Area: %04X - %04X\n"), f2->area_code[j], f2->end_service_code[j]);
         }            
 
-        printf("# Number of service code = %d\n", f2->num_service_code);
-        for (j = 0; j < f2->num_service_code; j++) {
+        _tprintf(_T("# Number of service code = %d\n"), f2->num_service_code);
+        for (j = 0; j < f2->num_service_code; j++)
+        {
             uint16 service = f2->service_code[j];
             printserviceinfo(service);
 
-            for (k = 0; k < 255; k++) {
+            for (k = 0; k < 255; k++)
+            {
                 uint8 data[16];
 
-                if (felica_read_without_encryption02(f2, service, 0, (uint8)k, data)) {
+                if (felica_read_without_encryption02(f2, service, 0, (uint8)k, data))
+                {
                     break;
                 }
                 
-                printf("%04X:%04X ", service, k);
+                _tprintf(_T("%04X:%04X "), service, k);
                 hexdump(data, 16);
-                printf("\n");
+                _tprintf(_T("\n"));
             }
         }
-        printf("\n");
+        _tprintf(_T("\n"));
         felica_free(f2);
     }
 
@@ -116,32 +130,35 @@ int _tmain(int argc, _TCHAR *argv[])
 
 static void printserviceinfo(uint16 s)
 {
-    char *ident;
+    TCHAR*  ident;
 
-    switch ((s >> 1) & 0xf) {
-    case 0: ident = "Area Code"; break;
-    case 4: ident = "Random Access R/W"; break; 
-    case 5: ident = "Random Access Read only"; break; 
-    case 6: ident = "Cyclic Access R/W"; break; 
-    case 7: ident = "Cyclic Access Read only"; break; 
-    case 8: ident = "Purse (Direct)"; break;
-    case 9: ident = "Purse (Cashback/decrement)"; break;
-    case 10: ident = "Purse (Decrement)"; break;
-    case 11: ident = "Purse (Read only)"; break;
-    default: ident = "INVALID or UNKOWN"; break;
+    switch ((s >> 1) & 0xf)
+    {
+        case 0: ident = _T("Area Code"); break;
+        case 4: ident = _T("Random Access R/W"); break; 
+        case 5: ident = _T("Random Access Read only"); break; 
+        case 6: ident = _T("Cyclic Access R/W"); break; 
+        case 7: ident = _T("Cyclic Access Read only"); break; 
+        case 8: ident = _T("Purse (Direct)"); break;
+        case 9: ident = _T("Purse (Cashback/decrement)"); break;
+        case 10: ident = _T("Purse (Decrement)"); break;
+        case 11: ident = _T("Purse (Read only)"); break;
+        default: ident = _T("INVALID or UNKOWN"); break;
     }
 
-    printf("# Serivce code = %04X : %s", s, ident);
-    if ((s & 0x1) == 0) {
-        printf(" (Protected)");
+    _tprintf(_T("# Serivce code = %04X : %s"), s, ident);
+    if ((s & 0x1) == 0)
+    {
+        _tprintf(_T(" (Protected)"));
     }
-    printf("\n");
+    _tprintf(_T("\n"));
 }
 
-static void hexdump(uint8 *addr, int n)
+static void hexdump(uint8* addr, int n)
 {
     int i;
-    for (i = 0; i < n; i++) {
-        printf("%02X ", addr[i]);
+    for (i = 0; i < n; i++)
+    {
+        _tprintf(_T("%02X "), addr[i]);
     }
 }
